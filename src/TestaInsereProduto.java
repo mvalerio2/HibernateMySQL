d59dad21b4a503d8ede36f8733c66bf871cf734f
rodrigolsmoraes <rodrigo.lsmoraes.rm@gmail.com>
2015-03-30 22:50:23 -0300

    exer 3

--- src/TestaInsereProduto.java
+++ src/TestaInsereProduto.java
@@ -9,9 +9,13 @@ Persistence.createEntityManagerFactory("lojavirtualDB");
 EntityManager em = factory.createEntityManager();
 Produto p = new Produto();
 p.setNome("camiseta");
-p.setPreco(12.23);
+p.setPreco(12.00);
+Produto p1 = new Produto();
+p1.setNome("shorts");
+p1.setPreco(15.00);
 em.getTransaction().begin();
 em.persist(p);
+em.persist(p1);
 em.getTransaction().commit();
 }
 }
